79060c4eb3c871545a4f121eea3ac586a8f91de3
Clément Bailly <87484828+Misterion11@users.noreply.github.com>
2021-07-23 19:50:49 +0200

    Create Annula.php

---
+++ Include/Annula.php
@@ -0,0 +1,3 @@
+<form class="changement" action="Reserver.php" method="post">
+  <legend class="modif">Voici les activités auquels vous êtes incrits : </legend>
+  <table class='presta'><tr><th class='presta'> Numéro d'Inscription </th><th class='presta'> Numéro de l'activité </th><th class='presta'> Date de l'inscription </th></tr>
